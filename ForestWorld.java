cd9507312dd5e3ad1a02f27398cbcb8c67be457a
Adam Beddoe <adam.beddoe@gmail.com>
2017-02-27 16:54:48 +0000

    Forrest world

---
+++ ForestWorld.java
@@ -0,0 +1,25 @@
+class ForrestWorld {
+    Region[][] world;
+    int sizeX, sizeY;
+
+    ForrestWorld(int worldX, int worldY){
+      this.sizeX = worldX;
+      this.sizeY = worldY;
+
+      this.world = new Region[this.sizeX][this.sizeY];
+
+      for(int i = 0; i<this.sizeX; i++){
+        for (int j = 0; j<this.sizeY; j++){
+          world[i][j] = new Region();
+        }
+      }
+    }
+
+    public void testData() {
+      for(int i = 0; i<sizeX; i++){
+        for (int j = 0; j<sizeY; j++){
+          world[i][j].onFire = false;
+        }
+      }
+    }
+}
