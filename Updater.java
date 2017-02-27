b1b4aa4c1c286fd19953f3d7ba8263f0225761c5
Adam Beddoe <adam.beddoe@gmail.com>
2017-02-27 22:45:29 +0000

    Fixed typo

--- Updater.java
+++ Updater.java
@@ -1,5 +1,5 @@
 class Updater extends Visitor {
-    public void visit(ForrestWorld f) {
+    public void visit(ForestWorld f) {
         for(int i = 0; i < f.sizeX; i++) {
             for(int j = 0; i < f.sizeY; j++) {
                 if (f.world[i][j].probabilityOfFire == 1) {
