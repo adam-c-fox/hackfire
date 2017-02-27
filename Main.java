3ebd7a432c554d90ff4bd157591a7015ab219c69
Adam Beddoe <adam.beddoe@gmail.com>
2017-02-27 19:35:01 +0000

    Added image importing

--- Main.java
+++ Main.java
@@ -10,5 +10,20 @@ class Main {
         testWorld.testData();
 
         System.out.println(testWorld.world[3][4].getPressure());
+        ImageImport.getImage();
+
+        PixelGrid p = new PixelGrid("fire.jpg", 2000, 2000);
+        ForrestWorld f = p.toForrestWorld();
+        for(int i = 0; i<2000; i++) {
+            for(int j = 0; j<2000; j++) {
+                System.out.print(toInt(f.world[i][j].onFire));
+            }
+            System.out.println();
+        }
+    }
+
+    private static int toInt(Boolean b) {
+        if (b == true) return 1;
+        else return 0;
     }
 }
