55896dafe3932b42c144d17e2876cab31e7e9be8
Adam Beddoe <adam.beddoe@gmail.com>
2017-02-27 15:40:12 +0000

    small changes

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        Datapoint test = new Datapoint(10, 10 ,10);
+        Datapoint test = new Datapoint(10.0, 10.0 ,10.0);
         System.out.println(test.getPressure());
         System.out.println(test.getWindVelocity());
         System.out.println("Hello World!");
