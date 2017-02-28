72db9581504603ee98e0a1ca56bcab668afbd822
ps16925 <ps16925@my.bristol.ac.uk>
2017-02-28 01:54:25 +0000

    Update Updater.java

--- Updater.java
+++ Updater.java
@@ -2,18 +2,18 @@ class Updater extends Visitor {
     public void visit(ForestWorld f) {
         for(int i = 0; i < f.sizeX; i++) {
             for(int j = 0; i < f.sizeY; j++) {
-                if (world[i][j].Color == stateColor[1]){
-                    if ( world[i][j+1].probabilityOfFire > 0.5){
-                        world[i][j+1].onFire = true;
+                if (f.world[i][j].onFire == true){
+                    if (f.world[i][j+1].probabilityOfFire > 0.5){
+                        f.world[i][j+1].onFire = true;
                     }
-                    if (world[i][j-1].probabilityOfFire > 0.5){
-                        world[i][j-1].onFire = true;
+                    if (f.world[i][j-1].probabilityOfFire > 0.5){
+                        f.world[i][j-1].onFire = true;
                     }
-                    if (world[i+1][j].probabilityOfFire > 0.5){
-                        world[i+1][j].onFire = true;
+                    if (f.world[i+1][j].probabilityOfFire > 0.5){
+                        f.world[i+1][j].onFire = true;
                     }
-                    if (world[i-1][j].probabilityOfFire > 0.5){
-                        world[i-1][j].onFire = true;
+                    if (f.world[i-1][j].probabilityOfFire > 0.5){
+                        f.world[i-1][j].onFire = true;
                     }
                 }
               
