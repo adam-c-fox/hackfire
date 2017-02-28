7be3eda07dd6975967533f283c95ce71bdda0b9b
Adam Fox <af16371@my.bristol.ac.uk>
2017-02-28 03:04:25 +0000

    Fixed bottom right fart

--- Updater.java
+++ Updater.java
@@ -1,23 +1,26 @@
 class Updater extends Visitor {
     public void visit(ForestWorld f) {
+      ForestWorld n = new ForestWorld(f.sizeX, f.sizeY);
+
         for(int i = 1; i < f.sizeX-1; i++) {
             for(int j = 1; j < f.sizeY-1; j++) {
                 if (f.world[i][j].onFire == true){
                     if (f.world[i][j+1].probabilityOfFire > 0.5){
-                        f.world[i][j+1].onFire = true;
+                        n.world[i][j+1].onFire = true;
                     }
                     if (f.world[i][j-1].probabilityOfFire > 0.5){
-                        f.world[i][j-1].onFire = true;
+                        n.world[i][j-1].onFire = true;
                     }
                     if (f.world[i+1][j].probabilityOfFire > 0.5){
-                        f.world[i+1][j].onFire = true;
+                        n.world[i+1][j].onFire = true;
                     }
                     if (f.world[i-1][j].probabilityOfFire > 0.5){
-                        f.world[i-1][j].onFire = true;
+                        n.world[i-1][j].onFire = true;
                     }
                 }
 
             }
         }
+        f.world = n.world;
     }
 }
