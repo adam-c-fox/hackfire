eab40801b04a1e6c31dd26b6dd68ce1c81ac490c
Adam Beddoe <adam.beddoe@gmail.com>
2017-02-28 09:49:58 +0000

    Added functionality for probability

--- ProbabilityCalculator.java
+++ ProbabilityCalculator.java
@@ -1,6 +1,24 @@
 class ProbabilityCalculator extends Visitor {
     @Override
     void visit(ForestWorld f) {
-        
-    }
+        ForestWorld n = new ForestWorld(f.sizeX, f.sizeY);
+        //n.world = f.world;
+        for(int j = 1; j < f.sizeY-1; j++){
+          for(int i = 1; i < f.sizeX-1; i++) {
+                  if (f.world[i][j+1].probabilityOfFire > 0.5 && f.world[i][j+1].charred == false) {
+                      n.world[i][j+1].probabilityOfFire = f.world[i][j].probabilityOfFire + f.world[i][j+1].probabilityOfFire;
+                  }
+                  if (f.world[i][j-1].probabilityOfFire > 0.5 && f.world[i][j-1].charred == false) {
+                      n.world[i][j-1].probabilityOfFire = f.world[i][j].probabilityOfFire + f.world[i][j-1].probabilityOfFire;
+                  }
+                  if (f.world[i+1][j].probabilityOfFire > 0.5 && f.world[i+1][j].charred == false) {
+                      n.world[i+1][j].probabilityOfFire = f.world[i][j].probabilityOfFire + f.world[i+1][j].probabilityOfFire;
+                  }
+                  if (f.world[i-1][j].probabilityOfFire > 0.5 && f.world[i-1][j].charred == false) {
+                      n.world[i-1][j].probabilityOfFire = f.world[i][j].probabilityOfFire + f.world[i-1][j].probabilityOfFire;
+                  }
+              }
+          }
+          f.world = n.world;
+     }
 }
