a04fee976eb6dd68ea283a0b57de0b636461f71d
ps16925 <ps16925@my.bristol.ac.uk>
2017-02-28 01:45:53 +0000

    Update Region.java

--- Region.java
+++ Region.java
@@ -1,13 +1,4 @@
 class Region {
     Boolean onFire;
     Double probabilityOfFire;
-    Color[] statecolor={Color.black,Color.white};
-    
-    Region(){
-        if (onFire == true){
-           Color = statecolor[1];
-        }else {
-           Color = statecolor[0];
-        }      
-    }
 }
